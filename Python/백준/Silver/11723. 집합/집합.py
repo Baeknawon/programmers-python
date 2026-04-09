f1de84b7433dc8407ca26c73f615e6dcbf78b4e9
Nawon Baek <146924040+Baeknawon@users.noreply.github.com>
2026-04-10 01:32:05 +0900

    [Silver V] Title: 집합, Time: 3088 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/11723. 집합/집합.py
+++ Python/백준/Silver/11723. 집합/집합.py
@@ -1,38 +1,21 @@
-S = set()
-def add(S,num):
-    S.add(num)
-def remove(S,num):
-    S.discard(num)
-    
-def check(S,num):
-    print(1 if num in S else 0)
-def toggle(S,num):
-    if num not in S:
-        S.add(num)
-    else:
-        S.discard(num)
-def all():
-    return set(range(1,21))
-def empty():
-    return set()
+#비트마스크
 import sys
 input = sys.stdin.readline
+
+S = 0
 N = int(input())
 for _ in range(N):
     cmd = input().split()
+
     if cmd[0] == 'add':
-        add(S,int(cmd[1]))
+        S |= (1 << int(cmd[1])) #cmd[1] 번째 자리에 1만들기(있다고 표시) |= -> 기존값에 합치기
     elif cmd[0] == 'remove':
-        remove(S,int(cmd[1]))
+        S &= ~(1 << int(cmd[1])) #cmd위치만 1로 바꾸고 반전, 그러면 cmd위치만 0(제거) 가 됨 &= -> 해당 자리만 0으로 만들기(제거)
     elif cmd[0] == 'check':
-        check(S,int(cmd[1]))
-    elif cmd[0] == 'toggle':
-        toggle(S,int(cmd[1]))
+        print(1 if S & ( 1 << int(cmd[1])) else 0) #S랑 cmd[1] 이랑 비교해서 0이면 없음 1이면 있음
+    elif cmd[0] =='toggle':
+        S ^= (1 << int(cmd[1])) #xor임. S랑 cmd 비교,1^1 = 0(제거), 0^1 = 1(추가)
     elif cmd[0] == 'all':
-        S = all()
-    elif cmd[0] == 'empty':
-        S = empty()
-        
-        
-    
-    
+        S = (1 << 21) -1 #21번째까지..100000..였는데 -1하면 1111...(다있는상태)
+    elif cmd[0] =='empty':
+        S = 0 #비우기
