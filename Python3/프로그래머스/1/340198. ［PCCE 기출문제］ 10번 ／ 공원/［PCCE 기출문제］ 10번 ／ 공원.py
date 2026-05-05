d9d53414731dc4a21541b37f2a50298d7f1b6ca0
Nawon Baek <146924040+Baeknawon@users.noreply.github.com>
2026-05-05 23:48:20 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 1.30 ms, Memory: 9.36 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
@@ -0,0 +1,29 @@
+def solution(mats, park):
+    n = len(park)
+    m = len(park[0])
+
+    dp = [[0]*m for _ in range(n)]
+
+    max_size = 0
+
+    for i in range(n):
+        for j in range(m):
+            if park[i][j] == "-1":
+                if i == 0 or j == 0:
+                    dp[i][j] = 1
+                else:
+                    dp[i][j] = min(
+                        dp[i-1][j],
+                        dp[i][j-1],
+                        dp[i-1][j-1]
+                    ) + 1
+
+                max_size = max(max_size, dp[i][j])
+
+    # mats 중에서 가능한 최대 찾기
+    mats.sort(reverse=True)
+    for size in mats:
+        if size <= max_size:
+            return size
+
+    return -1
