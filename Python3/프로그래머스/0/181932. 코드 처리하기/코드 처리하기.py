50939d28ebe03ed7054551607a203865b847dd40
Nawon Baek <146924040+Baeknawon@users.noreply.github.com>
2026-05-14 22:09:17 +0900

    [level 0] Title: 코드 처리하기, Time: 11.55 ms, Memory: 9.27 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,17 @@
+def solution(code):
+    mode = 0
+    ret =""
+    for i in range(len(code)):
+        if mode == 0:
+            if code[i] != '1' and i % 2 == 0:
+                ret += code[i]
+            elif code[i] == '1':
+                mode = 1 - mode
+        elif mode == 1:
+            if code[i] != '1' and i % 2 != 0:
+                ret += code[i]
+            elif code[i] == '1':
+                mode = 1 - mode
+    if not ret :
+        ret = "EMPTY"
+    return ret
