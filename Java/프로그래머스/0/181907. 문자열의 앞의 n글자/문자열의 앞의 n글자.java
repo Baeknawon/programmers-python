179d5eb4af13ff53c13a51334792ac468d26f6e7
Nawon Baek <146924040+Baeknawon@users.noreply.github.com>
2026-06-15 14:39:21 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 4.34 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        char[] ms = my_string.toCharArray();
+        for(int i = 0; i < n; i ++){
+            answer += ms[i];
+        }
+        return answer;
+    }
+}
